47e2ab65cd6919d42358ded81071345ce7d142df
antoniachroni <chronia@chop.edu>
2023-08-04 10:16:45 -0400

    Update function-create-violin-plot.R

--- analyses/tmb-vaf-longitudinal/util/function-create-violin-plot.R
+++ analyses/tmb-vaf-longitudinal/util/function-create-violin-plot.R
@@ -8,16 +8,16 @@ create_violin_plot <- function(tmb) {
   tmb$tumor_descriptor <- factor(x = tmb$tumor_descriptor, levels = c("Diagnosis", "Progressive", "Recurrence", "Deceased"))
 
   # Plot violin plot
-  p <- ggplot(tmb, aes(x = tumor_descriptor, y = log_tmb, fill = tumor_descriptor),
+  p <- ggplot(tmb, aes(x = tumor_descriptor, y = log2_mutation_count, fill = tumor_descriptor),
               palette = dxrelcol, alpha = 0.8,add = "boxplot",
               add.params = list(fill = "white")) +
                geom_violin(trim=TRUE) +
     scale_fill_manual(values = c("Diagnosis" = "dodgerblue3", "Progressive" = "gray", "Recurrence" = "#800080", "Deceased" = "firebrick3")) +
-               stat_compare_means(comparisons = list(c("Diagnosis", "Relapse")), label.y = c(15), label = "p.format")+ # Add significance levels
+               # stat_compare_means(comparisons = list(c("Diagnosis", "Relapse")), label.y = c(15), label = "p.format")+ # Add significance levels
                #ggpubr::stat_compare_means(label.y.npc = "top") + ##global p
                # facet_grid(~facet) +##global p
                theme_Publication() + 
-               xlab("timepoints_model") + ylab('log_tmb')+
+               xlab("") + ylab('log2[Total Mutations]')+
                scale_y_continuous(limits = c(0, 6))
   return(p)
   
