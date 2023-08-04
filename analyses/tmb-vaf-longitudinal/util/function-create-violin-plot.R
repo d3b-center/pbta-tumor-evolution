449d83358a5b2db6b2e7ab3750da7f7908f8de14
antoniachroni <chronia@chop.edu>
2023-08-03 22:41:24 -0400

    Edit script

--- analyses/tmb-vaf-longitudinal/util/function-create-violin-plot.R
+++ analyses/tmb-vaf-longitudinal/util/function-create-violin-plot.R
@@ -1,8 +1,8 @@
-# Create barplots 
-# This is a function to create correlation plots using VAFs 
+# Create violin plots 
+# This is a function to create violin plots using TMB 
 # between "Deceased" time point and any other "timepoint" (variable)
 # while considering multiple bs_id samples per each timepoint
-create_barplot <- function(tmb) {
+create_violin_plot <- function(tmb) {
 
  # Reorder time points
   tmb$tumor_descriptor <- factor(x = tmb$tumor_descriptor, levels = c("Diagnosis", "Progressive", "Recurrence", "Deceased"))
@@ -10,16 +10,17 @@ create_barplot <- function(tmb) {
 # Reorder timepoints model
   tmb$timepoints_model <- factor(x = tmb$timepoints_model, levels = c("dx", "pro", "rec", "dx_pro", "dx_rec", "pro_rec"))
 
-  # Plot stacked barplot 
-    p <- ggplot(tmb, aes(x = Kids_First_Participant_ID, y = mutation_count, fill = tumor_descriptor))+  
-      geom_col(position = position_stack(reverse = TRUE)) +
-        scale_fill_manual(values = c("Diagnosis" = "dodgerblue3", "Progressive" = "gray", "Recurrence" = "#800080", "Deceased" = "firebrick3")) +
-          geom_bar( stat="identity", width = 0.9) + 
-            theme_Publication() + 
-              theme(axis.text.x = element_text(angle = 85, hjust = 1, vjust =1)) + 
-                labs(title = paste("Stacked barplot", sep = " ")) + 
-                  labs(x = "Kids_First_Participant_ID", y = "Total Mutations") +
-                    ylim(0, 13000)
-                      return(p)
-                   
+  # Plot violin plot
+  p <- ggplot(tmb, aes(x = timepoints_model, y = log_tmb, fill = tumor_descriptor),
+              palette = dxrelcol, alpha = 0.8,add = "boxplot",
+              add.params = list(fill = "white")) +
+               geom_violin() +
+    scale_fill_manual(values = c("Diagnosis" = "dodgerblue3", "Progressive" = "gray", "Recurrence" = "#800080", "Deceased" = "firebrick3")) +
+               stat_compare_means(comparisons = list(c("Diagnosis", "Relapse")), label.y = c(15), label = "p.format")+ # Add significance levels
+               #ggpubr::stat_compare_means(label.y.npc = "top") + ##global p
+               # facet_grid(~facet) +##global p
+               theme_Publication() + 
+               xlab("timepoints_model") + ylab('log_tmb')+
+               scale_y_continuous(limits = c(0, 6))
+  
   }
