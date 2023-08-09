abe5f38e0551d982e41da17a776cf15a4e2bfd3a
antoniachroni <chronia@chop.edu>
2023-08-09 10:07:47 -0400

    Remove facet_wrap from function-create-stacked-barplot.R

--- analyses/tmb-vaf-longitudinal/util/function-create-stacked-barplot.R
+++ analyses/tmb-vaf-longitudinal/util/function-create-stacked-barplot.R
@@ -12,7 +12,6 @@ create_stacked_barplot <- function(tmb, ylim) {
                geom_bar(stat ="identity", width = 0.9) + 
                theme_Publication() + 
                theme(axis.text.x = element_text(angle = 85, hjust = 1, vjust = 1)) + 
-               #facet_wrap(~cancer_group, ncol = 4) +
                labs(title = paste("Stacked barplot", sep = " ")) + 
                labs(x = "Kids_First_Participant_ID", y = "Total Mutations") +
                ylim(0, ylim))
