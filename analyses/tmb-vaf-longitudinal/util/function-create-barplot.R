945d0053186e2efe479d64302146c5b617f8f791
antoniachroni <chronia@chop.edu>
2023-08-17 14:18:29 -0400

    Clean R script based on Lint

--- analyses/tmb-vaf-longitudinal/util/function-create-barplot.R
+++ analyses/tmb-vaf-longitudinal/util/function-create-barplot.R
@@ -10,16 +10,25 @@
 create_stacked_barplot <- function(tmb_df, ylim) {
 
   # Reorder time points
-  tmb_df$tumor_descriptor <- factor(x = tmb_df$tumor_descriptor, levels = c("Diagnosis", "Progressive", "Recurrence", "Deceased", "Second Malignancy", "Unavailable"))
+  tmb_df$tumor_descriptor <- factor(x = tmb_df$tumor_descriptor, 
+                                    levels = c("Diagnosis", "Progressive", "Recurrence", "Deceased", "Second Malignancy", "Unavailable"))
 
+  # Define and order palette
+  palette <- tumor_descriptor_color_palette$hex_codes
+  names(palette) <- tumor_descriptor_color_palette$color_names
+  
   # Plot stacked barplot 
-  p <- print(ggplot(tmb_df, aes(x = Kids_First_Participant_ID, y = tmb, fill = tumor_descriptor)) +  
+  p <- print(ggplot(tmb_df, aes(x = Kids_First_Participant_ID, 
+                                y = tmb, 
+                                fill = tumor_descriptor)) +  
                geom_col(position = position_stack(reverse = TRUE)) +
-               scale_fill_manual(values = tumor_descriptor_color_palette$hex_codes) +
-               geom_bar(stat ="identity", width = 0.9) + 
+               scale_fill_manual(values = palette, breaks = sort(names(palette))) + 
+               geom_bar(stat = "identity", width = 0.9) + 
                theme_Publication() + 
-               theme(axis.text.x = element_text(angle = 85, hjust = 1, vjust = 1)) + 
-               labs(title = paste("Stacked barplot", sep = " ")) + 
+               theme(axis.text.x = element_text(angle = 85, 
+                                                hjust = 1, 
+                                                vjust = 1)) + 
+               labs(title = paste("TMB in PBTA cohort", sep = " ")) + 
                labs(x = "Kids_First_Participant_ID", y = "TMB") +
                ylim(0, ylim))
   return(p)
@@ -38,31 +47,38 @@ create_stacked_barplot <- function(tmb_df, ylim) {
 #' @export
 #'
 #' @examples
-create_stacked_barplot_cancer_group_sum <- function(tmb_df, ylim, ct_id) {
+create_stacked_barplot_ct <- function(tmb_df, ylim, ct_id) {
   
   # Reorder time points
-  tmb_df$tumor_descriptor <- factor(x = tmb_df$tumor_descriptor, levels = c("Diagnosis", "Progressive", "Recurrence", "Deceased", "Second Malignancy", "Unavailable"))
+  tmb_df$tumor_descriptor <- factor(x = tmb_df$tumor_descriptor, 
+                                    levels = c("Diagnosis", "Progressive", "Recurrence", "Deceased", "Second Malignancy", "Unavailable"))
+  
+  # Define and order palette
+  palette <- tumor_descriptor_color_palette$hex_codes
+  names(palette) <- tumor_descriptor_color_palette$color_names
   
   # Plot stacked barplot 
-  p <- print(ggplot(tmb_df, aes(x = patient_id, y = tmb, fill = tumor_descriptor)) +  
+  p <- print(ggplot(tmb_df, aes(x = patient_id, 
+                                y = tmb, 
+                                fill = tumor_descriptor)) +  
                geom_col(position = position_stack(reverse = TRUE)) +
-               scale_fill_manual(values = tumor_descriptor_color_palette$hex_codes) +
-               geom_bar(stat ="identity", width = 0.9) + 
+               scale_fill_manual(values = palette, breaks = sort(names(palette))) + 
+               geom_bar(stat = "identity", width = 0.9) + 
                theme_Publication() + 
-               theme(axis.text.x = element_text(angle = 85, hjust = 1, vjust = 1)) + 
-               labs(title = paste(ct_id, "Stacked barplot", sep = " ")) + 
+               theme(axis.text.x = element_text(angle = 85, 
+                                                hjust = 1, 
+                                                vjust = 1)) + 
+               labs(title = paste(ct_id)) + 
                labs(x = "Kids_First_Participant_ID", y = "TMB") +
                ylim(0, ylim))
   return(p)
 }
 
 ###--------------------------------------------------------------
-#' Create barplot for each `Kids_First_Participant_ID` and per `Kids_First_Biospecimen_ID` in the df
+#' Create barplot for each `Kids_First_Biospecimen_ID` in the df
 #'
 #' @param tmb_df 
 #' @param ylim
-#' @param sid 
-
 #'
 #' @return
 #' @export
@@ -71,17 +87,25 @@ create_stacked_barplot_cancer_group_sum <- function(tmb_df, ylim, ct_id) {
 create_barplot_sample <- function(tmb_df, ylim, sid) {
   
   # Reorder time points
-  tmb_df$tumor_descriptor <- factor(x = tmb_df$tumor_descriptor, levels = c("Diagnosis", "Progressive", "Recurrence", "Deceased", "Second Malignancy", "Unavailable"))
+  tmb_df$tumor_descriptor <- factor(x = tmb_df$tumor_descriptor, 
+                                    levels = c("Diagnosis", "Progressive", "Recurrence", "Deceased", "Second Malignancy", "Unavailable"))
+  
+  # Define and order palette
+  palette <- tumor_descriptor_color_palette$hex_codes
+  names(palette) <- tumor_descriptor_color_palette$color_names
   
   # Plot stacked barplot 
-  p <- print(ggplot(tmb_df, aes(x = Kids_First_Biospecimen_ID, y = mutation_count)) +  
-               geom_bar(stat ="identity", width = 0.9, color = tumor_descriptor_color_palette$hex_codes, fill = tumor_descriptor) + 
+  p <- print(ggplot(tmb_df, aes(x = Kids_First_Biospecimen_ID, 
+                                y = mutation_count, 
+                                fill = tumor_descriptor)) +  
                geom_col(position = position_stack(reverse = TRUE)) +
-               #scale_fill_manual(values = tumor_descriptor_color_palette$hex_codes) +
+               geom_bar(stat = "identity", width = 0.9) + 
+               scale_fill_manual(values = palette, breaks = sort(names(palette))) + 
                theme_Publication() + 
-               theme(axis.text.x = element_text(angle = 85, hjust = 1, vjust = 1)) + 
-               labs(title = paste(sid, "Barplot", sep = " ")) + 
+               theme(axis.text.x = element_text(angle = 85, 
+                                                hjust = 1, 
+                                                vjust = 1)) + 
+               labs(title = paste(sid)) + 
                labs(x = "Kids_First_Biospecimen_ID", y = "Total Mutations") +
                ylim(0, ylim))
-  return(p)
 }
