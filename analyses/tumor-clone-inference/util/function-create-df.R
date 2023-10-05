a2a6b5f155ec604b4314b9221c9d99a6c1ccc4d8
antoniachroni <chronia@chop.edu>
2023-10-05 00:50:36 -0400

    Add function create-df in util

---
+++ analyses/tumor-clone-inference/util/function-create-df.R
@@ -0,0 +1,33 @@
+#' Create df
+#'
+#' @param df 
+#' @param samples 
+#' 
+#'
+#' @return
+#' @export
+#'
+#' @examples
+create_df <- function(df, samples) {
+  # Reference df
+  ref_df <- df_sub %>%
+    filter(td_bs_id == td_bs_id_names) %>%
+    mutate(td_bs_id = paste(td_bs_id_names, "ref", sep = "_")) %>% 
+    pivot_wider(names_from = "td_bs_id", values_from = "XX_ref") %>% 
+    select(!XX_alt)
+  
+  # Alteration df
+  alt_df <- df_sub %>%
+    filter(td_bs_id == td_bs_id_names) %>%
+    mutate(td_bs_id = paste(td_bs_id_names, "alt", sep = "_")) %>% 
+    pivot_wider(names_from = "td_bs_id", values_from = "XX_alt") %>%
+    select(!XX_ref)
+  
+  # Merge df
+  merge_df <- ref_df %>% 
+    full_join(alt_df, by = c("cg_id_kids", "mut_id", "Chr", "Position", "Wild", "Mut")) %>% 
+    as.data.frame() 
+  
+  return(merge_df)
+}
+
