6fdbe9b17566149dbe5e02e6ca03a0891d943373
antoniachroni <chronia@chop.edu>
2023-10-05 01:16:29 -0400

    Fix typo

--- analyses/tumor-clone-inference/util/function-create-df.R
+++ analyses/tumor-clone-inference/util/function-create-df.R
@@ -11,14 +11,14 @@
 create_df <- function(df, samples) {
   # Reference df
   ref_df <- df_sub %>%
-    filter(td_bs_id == td_bs_id_names) %>%
+    #filter(td_bs_id == td_bs_id_names) %>%
     mutate(td_bs_id = paste(td_bs_id_names, "ref", sep = ":")) %>% 
     pivot_wider(names_from = "td_bs_id", values_from = "XX:ref") %>% 
     select(-c("XX:alt"))
   
   # Alteration df
   alt_df <- df_sub %>%
-    filter(td_bs_id == td_bs_id_names) %>%
+    #filter(td_bs_id == td_bs_id_names) %>%
     mutate(td_bs_id = paste(td_bs_id_names, "alt", sep = ":")) %>% 
     pivot_wider(names_from = "td_bs_id", values_from = "XX:alt") %>% 
     select(-c("XX:ref"))
