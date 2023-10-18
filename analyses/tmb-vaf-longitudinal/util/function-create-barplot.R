3cfc8c46eb8da0d604a41e35b62bb30bc251f056
antoniachroni <chronia@chop.edu>
2023-10-18 11:38:57 -0400

    Add function create_stacked_barplot_variant_kids

--- analyses/tmb-vaf-longitudinal/util/function-create-barplot.R
+++ analyses/tmb-vaf-longitudinal/util/function-create-barplot.R
@@ -91,7 +91,7 @@ create_stacked_barplot_variant <- function(count_df, x_value, palette, title) {
   
   # Plot stacked barplot 
   p <- print(ggplot(count_df,
-               aes(x =  x_value,
+               aes(x = x_value,
                    y = n, 
                    fill = Variant_Classification)) +  
           geom_bar(stat = "identity", position = "fill") +
@@ -151,3 +151,43 @@ create_stacked_barplot_variant_cg_id <- function(count_df, x_value, palette, tit
 }
 
 
+
+#' Create stacked barplots to explore variant types per cancer_group
+#'
+#' @param count_df 
+#' @param x_value 
+#' @param palette 
+#' @param title
+#' @param rows
+
+#' 
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#' 
+#'  
+#' 
+create_stacked_barplot_variant_kids <- function(count_df, x_value, palette, title, rows) {
+  
+  # Plot stacked barplot 
+  p <- print(ggplot(count_df,
+                    aes(x = x_value,
+                        y = n, 
+                        fill = Variant_Classification)) + 
+               geom_bar(stat = "identity", position = "fill") +
+               scale_fill_manual(values = palette, breaks = sort(names(palette))) + 
+               theme_Publication() + 
+               theme(axis.text.x = element_text(angle = 85,
+                                                hjust = 1,
+                                                vjust = 1),
+                     text = element_text(size = 14)) +
+               scale_y_continuous(labels = function(x) paste0(100*x/max(x),'%')) +
+               facet_wrap(~Kids_First_Participant_ID, scales = "free_x", nrow = rows) +
+               labs(title = title, 
+                    x = "Timepoint", 
+                    y = "Count"))
+  
+  return(p)
+}
