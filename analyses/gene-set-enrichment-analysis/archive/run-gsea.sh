691e0edcc03eaaae579399a5e29ada146b94811d
antoniachroni <chronia@chop.edu>
2023-11-01 12:04:39 -0400

    Add input, output and script files for gsea module

---
+++ analyses/gene-set-enrichment-analysis/archive/run-gsea.sh
@@ -0,0 +1,49 @@
+#########################################################################
+# Stephanie J. Spielman for ALSF CCDL 2020
+#
+# Run the GSEA pipeline:
+## 1. `01-conduct-gsea-analysis.R` to calculate scores
+## 2. `02-exploratory-gsea.Rmd` to explore the scores, lightly for now
+#
+# Usage: bash run-gsea.sh
+#
+# Takes one environment variable, `OPENPBTA_TESTING`, which is 1 for running
+# samples in CI for testing, 0 for running the full dataset (Default)
+#
+# Takes one environment variable, `BASE_SUBTYPING`, if value is 1 then
+# only runs modules required for subtyping if value is 0 runs all modules (Default)
+#########################################################################
+
+
+set -e
+set -o pipefail
+
+IS_CI=${OPENPBTA_TESTING:-0}
+RUN_FOR_SUBTYPING=${OPENPBTA_BASE_SUBTYPING:-0}
+
+# This script should always run as if it were being called from
+# the directory it lives in.
+script_directory="$(perl -e 'use File::Basename;
+  use Cwd "abs_path";
+  print dirname(abs_path(@ARGV[0]));' -- "$0")"
+cd "$script_directory" || exit
+
+DATA_DIR="../../data"
+RESULTS_DIR="results"
+
+if [[ "$RUN_FOR_SUBTYPING" -eq "0" ]]; then
+  HIST_FILE="${DATA_DIR}/histologies.tsv"
+else
+  HIST_FILE="${DATA_DIR}/histologies-base.tsv"
+fi
+
+######## Calculate scores from expression data ############
+INPUT_FILE="${DATA_DIR}/gene-expression-rsem-tpm-collapsed.rds"
+OUTPUT_FILE="${RESULTS_DIR}/gsva_scores.tsv"
+Rscript --vanilla 01-conduct-gsea-analysis.R --input ${INPUT_FILE} --output ${OUTPUT_FILE} --histology ${HIST_FILE}
+
+if [[ "$RUN_FOR_SUBTYPING" -eq "0" ]]; then
+  ######## Model GSVA scores ############
+  # Only run when histologies.tsv is generated which has cancer_group
+  Rscript -e "rmarkdown::render('02-model-gsea.Rmd', clean = TRUE, params=list(is_ci = ${IS_CI}))"
+fi
---
+++ analyses/gene-set-enrichment-analysis/util/function-create-plots.R
@@ -0,0 +1,232 @@
+#' Create barplot
+#'
+#' @param df
+#' @param y_value 
+#' @param title_value 
+#' @param rna_library 
+#' @param td_model_id
+#' @param cgGFAC
+#' 
+#'
+#' @return
+#' @export
+#'
+#' @examples
+create_barplot <- function(df, y_value, title_value, rna_library, td_model_id, cgGFAC) {
+  
+  p <- print(ggplot(df, aes(x = hallmark_name, 
+                            y = y_value,
+                            fill = y_value)) +  
+               geom_col(position = position_stack(reverse = TRUE)) +
+               rotate() +
+               geom_bar(stat = "identity", width = 0.5) + 
+               scale_fill_gradient(low = "blue", high = "red") +
+               theme_Publication() + 
+               theme(axis.text.x = element_text(angle = 85, 
+                                                hjust = 1, 
+                                                vjust = 1)) + 
+               # ggplot2::facet_wrap(~comparison, ncol = 3) +
+               labs(title = paste0(cgGFAC, " ", td_model_id, " Barplot for ", rna_library, " using ", title_value)) + 
+               labs(x = "", y = "Count") +
+               ylim(0, 1)) 
+  
+  return(p)
+  
+}
+
+
+
+
+
+#' Create diverging barplot
+#'
+#' @param df
+#' @param y_value 
+#' @param title_value 
+#' @param rna_library 
+#' @param td_model_id
+#' @param cgGFAC
+#' 
+#'
+#' @return
+#' @export
+#'
+#' @examples
+create_diverging_barplot <- function(df, y_value, title_value, rna_library, td_model_id, cgGFAC) {
+  
+  
+  p <- ggplot(df, aes(x = reorder(hallmark_name, y_value), 
+                      y = y_value,
+                      fill = y_value)) +  
+    geom_col(position = position_stack(reverse = TRUE)) +
+    rotate() +
+    geom_bar(stat = "identity", width = 0.5) + 
+    scale_fill_gradient(low = "blue", high = "red") +
+    theme_Publication() + 
+    theme(axis.text.x = element_text(angle = 85, 
+                                     hjust = 1, 
+                                     vjust = 1)) + 
+    # ggplot2::facet_wrap(~comparison, ncol = 3) +
+    labs(title = paste0(cgGFAC, " ", td_model_id, " diverging plot for ", rna_library, " using ", title_value)) + 
+    labs(x = "", y = "Pathway score") 
+  
+  return(p)
+  
+}
+
+
+
+
+#' Create dotplot
+#'
+#' @param df
+#' @param x_value 
+#' @param y_value 
+#' @param title_value 
+#' @param rna_library 
+#' @param td_model_id
+#' @param cgGFAC
+
+#' 
+#'
+#' @return
+#' @export
+#'
+#' @examples
+create_dotplot <- function(df, x_value, y_value, title_value, rna_library, td_model_id, cgGFAC) {
+  
+  p <- print(ggplot(df, mapping = aes(x = reorder(x_value, y_value), y_value)) +  
+               # geom_dotplot(binwidth = 1.5)  +
+               geom_line(aes(group = x_value)) +
+               geom_point(aes(color = y_value, size = y_value)) +
+               rotate() +
+               scale_color_gradient(low = "blue", high = "red") +
+               #scale_fill_manual(values = palette, breaks = sort(names(palette))) + 
+               theme_Publication() + 
+               theme(axis.text.x = element_text(angle = 85, 
+                                                hjust = 1, 
+                                                vjust = 1)) + 
+               #ggplot2::facet_wrap(~comparison, ncol = 3) +
+               labs(title = paste0(cgGFAC, " ", td_model_id, " Dotplot for ", rna_library, " using ", title_value)) + 
+               labs(x = "", y = "p_value") +
+               ylim(0, 1)) 
+  
+  return(p)
+  
+}
+
+
+#' Create volcano plot
+#'
+#' @param df
+#' @param title_value 
+#' @param rna_library 
+#' @param td_model_id
+#' @param cgGFAC
+
+#' 
+#'
+#' @return
+#' @export
+#'
+#' @examples
+create_volcano_plot <- function(df, title_value, rna_library, td_model_id, cgGFAC) {
+  
+  # make corplot reproducible when using the geom_text_repel function
+  set.seed(2023)
+  
+  labels_colors = c("bottom_ranked_data" = "#00AFBB", "Not significant" = "grey", "top_ranked_data" = "#bb0c00")
+  
+  # plot adding up all layers we have seen so far
+  p <- ggplot(data = df, aes(x  = pathway_score_difference, y = tukey_p_value, col = path_diff, label = labels)) +
+    geom_point(size = 6, fill = 4, alpha = 1 / 2) + 
+    theme_Publication() + 
+    geom_text_repel(point.padding = 0.2, # additional padding around each point
+                    box.padding = 0.5) +
+    scale_color_manual(values = labels_colors) + # to set the colours of our variable
+    # labels = c("bottom_ranked_data", "Not significant", "top_ranked_data")) + # to set the labels in case we want to overwrite the categories from the dataframe (UP, DOWN, NO)
+    #geom_vline(xintercept=c(-0.6, 0.6), col = "red", col = "gray", linetype = "dashed") +
+    geom_hline(yintercept = 0.05, col = "black", linetype = "dashed") +
+    #coord_cartesian(ylim = c(0, 250), xlim = c(-10, 10)) + # since some genes can have minuslog10padj of inf, we set these limits
+    # # Note. with coord_cartesian() even if we have genes with p-values or log2FC ourside our limits, they will still be plotted.
+    # labs(color = "path_diff")  #legend_title
+    labs(title = paste0(cgGFAC, " ", td_model_id, " plot for ", rna_library, " using ", title_value)) + 
+    labs(x = "Pathway score", y = "tukey_p_value")
+  
+  return(p)
+  
+}
+
+
+
+#' Create volcano plot
+#'
+#' @param df
+#' @param x_value
+#' @param y_value
+#' @param title_value 
+#' @param rna_library 
+#' @param td_model_id
+#' @param cgGFAC
+
+#' 
+#'
+#' @return
+#' @export
+#'
+#' @examples
+create_volcano_plot_log10 <- function(df, x_value, y_value, title_value, rna_library, td_model_id, cgGFAC) {
+  
+  # make corplot reproducible when using the geom_text_repel function
+  set.seed(2023)
+  
+  labels_colors = c("bottom_ranked_data" = "#00AFBB", "Not significant" = "grey", "top_ranked_data" = "#bb0c00")
+  
+  
+  # plot adding up all layers we have seen so far
+  p <- ggplot(data = df, aes(x  = x_value, y = y_value, col = path_diff, label = labels)) +
+    geom_point(size = 6, fill = 4, alpha = 1 / 2) + 
+    theme_Publication() + 
+    geom_text_repel(point.padding = 0.2, # additional padding around each point
+                    box.padding = 0.5) +
+    scale_color_manual(values = labels_colors) + # to set the colours of our variable
+                      # labels = c("bottom_ranked_data", "Not significant", "top_ranked_data")) + # to set the labels in case we want to overwrite the categories from the dataframe (UP, DOWN, NO)
+    #geom_vline(xintercept=c(-0.6, 0.6), col = "red", col = "gray", linetype = "dashed") +
+    geom_hline(yintercept = -log10(0.05), col = "black", linetype = "dashed") +
+    #coord_cartesian(ylim = c(0, 250), xlim = c(-10, 10)) + # since some genes can have minuslog10padj of inf, we set these limits
+    # # Note. with coord_cartesian() even if we have genes with p-values or log2FC ourside our limits, they will still be plotted.
+    # labs(color = "path_diff")  #legend_title
+    labs(title = paste0(cgGFAC, " ", td_model_id, " Volcano plot for ", rna_library, " using ", title_value)) + 
+    labs(x = "log2 Pathway score", y = "-log10_tukey_p_value")
+  
+  return(p)
+  
+}
+
+
+############## TO REMOVE? ############################
+# Create enhanced volcaon plot example
+# labels didnt work 
+#library(EnhancedVolcano)
+
+#p <- EnhancedVolcano(df,
+#                     lab = rownames(df),
+ #                    x = 'pathway_score_difference',
+ #                    y = 'tukey_p_value',
+ #                    selectLab = df$labels, 
+ #                    title = 'Volcano plot',
+ #                    pCutoff = 0.05,
+ #                    FCcutoff = 0.5,
+ #                    pointSize = 3.0,
+  #                   labSize = 6.0,
+ #                    labCol = 'black',
+#                     labFace = 'bold',
+ #                    boxedLabels = TRUE,
+#                     colAlpha = 4/5,
+#                     legendPosition = 'right',
+#                     legendLabSize = 14,
+#                     legendIconSize = 4.0,
+#                     drawConnectors = TRUE,
+#                     widthConnectors = 1.0,
+#                     colConnectors = 'black')
+
