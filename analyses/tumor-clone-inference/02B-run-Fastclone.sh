7fb87f038ba559b8f489cf583b4f33d9f317b763
antoniachroni <chronia@chop.edu>
2023-12-09 14:28:14 -0500

    Update 02B-run-Fastclone.sh

--- analyses/tumor-clone-inference/02B-run-Fastclone.sh
+++ analyses/tumor-clone-inference/02B-run-Fastclone.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # FastClone for PBTA Tumor evolution
-# Antonia Chroni
+# Antonia Chroni <chronia@chop.edu> for D3B
 #
 # Set this so the whole loop stops if there is an error
 set -e
@@ -10,7 +10,7 @@ set -o pipefail
 # Download the last version of FastClone
 # set directory
 # clone git repo in there
-git clone https://github.com/gstecher/CloneFinderAPI.git
+git clone https://github.com/GuanLab/FastClone_GuanLab.git
 mkdir ./FastClone_GuanLab
 
 # cd "$(dirname "${BASH_SOURCE[0]}")"
@@ -24,7 +24,7 @@ cd ./FastClone_GuanLab
 # $2 out_path: /Users/chronia/CHOP/projects/pbta-tumor-evolution/pbta-tumor-evolution-analysis/analyses/fishplot/results/distinct_samples_list.csv
 
 
-############################ Run FastClone_GuanLab script ######################################################################
+############################ Run FastClone ######################################################################
 #python clonefinder.py snv ./CloneFinder_input/"$1".txt
 
 fastclone load-pyclone prop ./fastclone-input/PT_KTRJ8TFY.tsv None solve ./fastclone-output/PT_KTRJ8TFY
@@ -35,7 +35,7 @@ fastclone load-pyclone prop ./fastclone-input/PT_KZ56XHJT.tsv None solve ./fastc
 # PT_Z4BF2NSB.tsv
 fastclone load-pyclone prop ./fastclone-input/PT_Z4BF2NSB.tsv None solve ./fastclone-output/PT_Z4BF2NSB
 
-############################ Remove input files from FastClone_GuanLab dir ###################################################
+############################ Remove input files from `FastClone_GuanLab`` dir ###################################################
 
 # rm files 
 
