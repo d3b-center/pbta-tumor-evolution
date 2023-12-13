bbff448c4014678bc08763982a80f4919c0868ce
antoniachroni <chronia@chop.edu>
2023-12-13 12:30:49 -0500

    Re-organize and archive files

--- analyses/tumor-clone-inference/02B-run-Fastclone.sh
+++ analyses/tumor-clone-inference/02B-run-Fastclone.sh
@@ -23,17 +23,15 @@ cd ./FastClone_GuanLab
 # $1 path to input files for FastClone_GuanLab: /Users/chronia/CHOP/projects/pbta-tumor-evolution/pbta-tumor-evolution-analysis/analyses/fishplot/results/distinct_samples_list.csv
 # $2 out_path: /Users/chronia/CHOP/projects/pbta-tumor-evolution/pbta-tumor-evolution-analysis/analyses/fishplot/results/distinct_samples_list.csv
 
-
 ############################ Run FastClone ######################################################################
-#python clonefinder.py snv ./CloneFinder_input/"$1".txt
 
-fastclone load-pyclone prop ./fastclone-input/PT_KTRJ8TFY.tsv None solve ./fastclone-output/PT_KTRJ8TFY
+fastclone load-pyclone prop /Users/chronia/CHOP/GitHub/pbta-tumor-evolution/analyses/tumor-clone-inference/results/fastclone-input-non-synonymous/PT_KTRJ8TFY.tsv None solve /Users/chronia/CHOP/GitHub/pbta-tumor-evolution/analyses/tumor-clone-inference/results/fastclone-output/PT_KTRJ8TFY
 
 # PT_KZ56XHJT.tsv
-fastclone load-pyclone prop ./fastclone-input/PT_KZ56XHJT.tsv None solve ./fastclone-output/PT_KZ56XHJT
+fastclone load-pyclone prop /Users/chronia/CHOP/GitHub/pbta-tumor-evolution/analyses/tumor-clone-inference/results/fastclone-input-non-synonymous/PT_KZ56XHJT.tsv None solve /Users/chronia/CHOP/GitHub/pbta-tumor-evolution/analyses/tumor-clone-inference/results/fastclone-output/PT_KZ56XHJT
 
 # PT_Z4BF2NSB.tsv
-fastclone load-pyclone prop ./fastclone-input/PT_Z4BF2NSB.tsv None solve ./fastclone-output/PT_Z4BF2NSB
+fastclone load-pyclone prop /Users/chronia/CHOP/GitHub/pbta-tumor-evolution/analyses/tumor-clone-inference/results/fastclone-input-non-synonymous/PT_Z4BF2NSB.tsv None solve /Users/chronia/CHOP/GitHub/pbta-tumor-evolution/analyses/tumor-clone-inference/results/fastclone-output/PT_Z4BF2NSB
 
 ############################ Remove input files from `FastClone_GuanLab`` dir ###################################################
 
